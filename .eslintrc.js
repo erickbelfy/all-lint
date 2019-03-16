0a5e5540a94ad4b75639eca60f6271557cc2b5a3
Erick Belfort <erick.belfort@onespan.com>
2019-03-16 18:47:08 -0400

    removing duplicated rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,45 +5,11 @@ module.exports = {
         "prettier/@typescript-eslint",
     ],
     "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        // Can I remove these now?
-        "ecmaFeatures": {
-        "impliedStrict": true,
-        "classes": true
-        }
-    },
-    "env": {
-        "browser": true,
-        "node": true,
-        "jquery": true,
-        "jest": true
-    },
     "plugins": [
         "@typescript-eslint",
         "prettier",
         "react",
         "react-hooks",
         "standard"
-    ],
-    "rules": {
-        "quotes": [
-            2,
-            "single",
-            {
-                "avoidEscape": true,
-                "allowTemplateLiterals": true
-            }
-            ],
-            "prettier/prettier": [
-            "error",
-            {
-                "trailingComma": "es5",
-                "singleQuote": true,
-                "printWidth": 80,
-            }
-            ],
-            "react-hooks/rules-of-hooks": "error",
-            "react-hooks/exhaustive-deps": "warn"
-    }
+    ]
 };
