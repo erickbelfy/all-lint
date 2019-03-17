638bee86af8ae1a89b7298488daccc12dbe3c684
Erick Belfort <erick.belfort@onespan.com>
2019-03-17 12:10:49 -0400

    Add
    - Modular files to improve reading
    - This commit will improve the way we define the extentions of our
    projects
    - it will improve readability also improve the way we can import the
    rules based on different project we might have

---
+++ @typescript-eslint.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rules: {
+    "@typescript-eslint/indent": "off",
+    "@typescript-eslint/member-delimiter-style": "off",
+    "@typescript-eslint/type-annotation-spacing": "off"
+  }
+};
