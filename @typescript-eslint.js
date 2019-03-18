9d5993048fa3e6c87f42fd19a818a0bea2d4c366
Erick Belfort <erick.belfort@onespan.com>
2019-03-18 11:13:12 -0400

    adding destructuring assignment to react linter

--- @typescript-eslint.js
+++ @typescript-eslint.js
@@ -1,7 +1,7 @@
 module.exports = {
   rules: {
-    "@typescript-eslint/indent": "off",
-    "@typescript-eslint/member-delimiter-style": "off",
-    "@typescript-eslint/type-annotation-spacing": "off"
-  }
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/type-annotation-spacing': 'off',
+  },
 };
--- react.js
+++ react.js
@@ -7,6 +7,7 @@ module.exports = {
     'react/forbid-prop-types': 0,
     'react/no-unescaped-entities': 0,
     'react/require-default-props': 0,
+    'react/destructuring-assignment': 1,
     'react/jsx-filename-extension': [
       1,
       {
